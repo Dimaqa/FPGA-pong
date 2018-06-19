27affe96128ad0fb83fc72c3ded4d7aeab034a84
Dimaqa <best2010a@ya.ru>
2018-06-19 20:51:17 +0300

    added pause and fixed some logic

--- main.v
+++ main.v
@@ -12,7 +12,7 @@ reg [9:0] ballX;
 reg [8:0] ballY;
 reg [8:0] PaddlePosition;
 
-initial ballX = 300;
+initial ballX = 310;
 initial ballY= 456;
 initial PaddlePosition = 255;
 
@@ -20,9 +20,9 @@ reg    [31:0]VGA_CLK_o;
 assign VGA_CLK        =VGA_CLK_o[0];
 always @(posedge CLOCK_50) VGA_CLK_o=VGA_CLK_o+1;
 
-	wire inDisplayArea;
-	wire [11:0] CounterX;
-	wire [11:0] CounterY;
+wire inDisplayArea;
+wire [11:0] CounterX;
+wire [11:0] CounterY;
 	hvsync_generator vga0(
 		.pixel_clk(VGA_CLK),
 		.h_fporch (16),
@@ -43,7 +43,14 @@ always @(posedge CLOCK_50) VGA_CLK_o=VGA_CLK_o+1;
 wire paddle = (CounterX>=PaddlePosition+8) && (CounterX<=PaddlePosition+120) && (CounterY[8:3]==59);
 wire border = (CounterX[9:3]==0) || (CounterX[9:3]==79) || (CounterY[8:3]==0) ; 
 wire BouncingObject = border | paddle;
-//
+
+localparam stpoint = 180;
+wire p =  CounterX >= stpoint +   0 && CounterX <= stpoint +   5 &&  CounterY >= 100 && CounterY <= 130 || CounterX >= stpoint + 5 &&  CounterX <= stpoint + 25 && ( CounterY >= 100 && CounterY <=105 || CounterY >=115 && CounterY <=120) ||(CounterX>= stpoint + 20 && CounterX <= stpoint + 25 && CounterY>=100 && CounterY <=120 );
+wire a = (CounterX >= stpoint +  60 && CounterX <= stpoint +  65 ||  CounterX >= stpoint + 80 && CounterX <= stpoint + 85) && CounterY >= 100 && CounterY <= 130 || CounterX >= stpoint +60 &&  CounterX<=stpoint +85 && ( CounterY >=100 && CounterY <=105 || CounterY >=115 && CounterY <=120);
+wire u = (CounterX >= stpoint + 120 && CounterX <= stpoint + 125 ||  CounterX >= stpoint + 140 && CounterX <= stpoint + 145) && CounterY >= 100 && CounterY <= 130 || CounterX >= stpoint + 125 &&  CounterX<= stpoint +145 &&  CounterY >=125 && CounterY <=130;
+wire s =  CounterX >= stpoint + 180 && CounterX <= stpoint + 205 && (CounterY >= 100 && CounterY <=105 || CounterY >= 115 && CounterY <=120 || CounterY >= 125 && CounterY <= 130) || CounterX >= stpoint + 180 &&  CounterX<= stpoint + 185 && CounterY >=105 && CounterY <=115 || CounterX >= stpoint + 200 &&  CounterX<= stpoint + 205 && CounterY >=115 && CounterY <=125;
+wire e =  CounterX >= stpoint + 240 && CounterX <= stpoint + 265 && (CounterY >= 100 && CounterY <=105 || CounterY >= 115 && CounterY <=120 || CounterY >= 125 && CounterY <= 130) || CounterX >= stpoint + 240 && CounterX <= stpoint + 245 && CounterY >= 100 && CounterY <= 130;
+wire pause = (p | a | u | s | e ) & !start;
 
 //speed of ball and start/pause
 reg [17:0]speed;
@@ -64,10 +71,10 @@ always @(posedge CLOCK_50)
 begin
 	clock06s = clock06s + 1;
 	if(clock06s == 0)
-		if(boost != 6'd63)
-			if(~KEY[0] | ~ KEY[1])
+		if(boost != 5'd31)
+			if(~KEY[0] & KEY[1] | KEY[0] & ~KEY[1] )
 				boost = boost + 1;
-			else 
+			else
 				boost = 1;			
 end
 always @(posedge nextpos)
@@ -78,6 +85,7 @@ begin
 	if(start)
 	begin
       if(~KEY[0]) 
+      
 		if(PaddlePosition  > (9'd511 - boost)) 
 			PaddlePosition = 9'd511;
 		else
@@ -116,7 +124,7 @@ begin
 	//reset if out of field
 	if(ballY > 460)
 	begin
-		ballY = 456; ballX = 300; over = 1;
+		ballY = 456; ballX = 310; over = 1;
 	end
 	//else change coord
 	else
@@ -124,13 +132,16 @@ begin
 		over = 0;
 		if(start)
 		begin
-			if(~(CollisionX1 & CollisionX2))        // if collision on both X-sides, don't move in the X direction
+			if(~(CollisionX1 & CollisionX2))       
 			begin
+			if(CollisionY2) //&& ballX+boost < 9'b1001111000 && ballX-boost > 3'b111 )
+				ballX <= ballX + (ball_dirX ? -boost : boost);
+			else
 				ballX <= ballX + (ball_dirX ? -1 : 1);
 				if(CollisionX2) ball_dirX <= 1; else if(CollisionX1) ball_dirX <= 0;
 			end
 
-			if(~(CollisionY1 & CollisionY2))        // if collision on both Y-sides, don't move in the Y direction
+			if(~(CollisionY1 & CollisionY2))        
 			begin
 				ballY <= ballY + (ball_dirY ? -1 : 1);
 				if(CollisionY2) ball_dirY <= 1; else if(CollisionY1) ball_dirY <= 0;
@@ -138,10 +149,11 @@ begin
 		end
 	end
 end 
+
 //define VGA outputs
-wire R = BouncingObject | ball;
-wire G = BouncingObject ;//| ball;
-wire B = BouncingObject ;//| ball;
+wire R = BouncingObject | ball | pause;
+wire G = BouncingObject;
+wire B = BouncingObject;
 
 assign  VGA_R = {4{R}} & {4{inDisplayArea}};
 assign  VGA_G = {4{G}} & {4{inDisplayArea}};
